cbdc64ee811d77ed26d24fb78f357f0aa3232fcb
Бельский Илья < >
2016-11-28 18:33:44 +0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,18 @@
 
 'use strict';
 
-const gulp          = require('gulp'),
+const gulp = require('gulp'),
     gulpLoadPlugins = require('gulp-load-plugins'),
-    del             = require('del'),
-    browserSync     = require('browser-sync').create(),
-    plugins         = gulpLoadPlugins(),
-    Pageres         = require('pageres'),
-    reload          = browserSync.reload,
-    mainBowerFiles  = require('main-bower-files'),
-    nameProject     = 'nameProject',
-    siteScreen      = 'http://localhost:3000',
-    resolutions     = ['iphone 4', '1920x1080'];
+    del = require('del'),
+    browserSync = require('browser-sync').create(),
+    plugins = gulpLoadPlugins(),
+    Pageres = require('pageres'),
+    reload = browserSync.reload,
+    psi = require('psi'),
+    mainBowerFiles = require('main-bower-files'),
+    nameProject = 'nameProject';
+
+
 
 /********************************************************************/
 /*PATH***************************************************************/
@@ -29,15 +30,16 @@ var path = {
         fonts: './dist/fonts/',
         json: './dist/json/',
         vendor: './dist/vendor/',
-        screen: './screen/'
+        imgSet: './dist/images/src-set/'
     },
     src: {
         html: './src/*.html',
         js: './src/js/*.js',
-        style:'./src/scss/*.scss',
+        style: './src/scss/*.scss',
         img: './src/images/**/*',
         json: './src/json/*.json',
-        fonts: './src/fonts/**/*'
+        fonts: './src/fonts/**/*',
+        imgSet: './src/images/src-set/**/*'
     },
     watch: {
         style: './src/scss/**/*',
@@ -45,63 +47,117 @@ var path = {
         html: './src/**/*.html',
         img: './src/images/**/*',
         json: './src/json/**/*',
-        vendor: 'bower.json'
+        vendor: 'bower.json',
+        imgSet: './src/images/src-set/**/*'
     }
 };
 
 /********************************************************************/
 /*VENDOR-PATH*********************************************************/
 /********************************************************************/
-var bowerConfig  = {
+var bowerConfig = {
     "bootstrap": {
         "main": ['./scss/**/*', './dist/js/bootstrap.min.js']
     },
-    "jquery" : {
+    "jquery": {
         "main": './dist/jquery.min.js'
     },
-    "tether" : {
-        "main" : './dist/js/tether.min.js'
+    "tether": {
+        "main": './dist/js/tether.min.js'
     },
-    "font-awesome" : {
-        "main" : ['./fonts/**/*', './css/font-awesome.min.css']
+    "font-awesome": {
+        "main": ['./fonts/**/*', './css/font-awesome.min.css']
+    },
+
+    "bourbon": {
+        "main": './app/assets/**/*'
+    },
+
+    "magnific-popup": {
+        "main": ['./dist/jquery.magnific-popup.min.js', './dist/magnific-popup.css']
+    },
+
+    "owl.carousel": {
+        "main": ['./dist/assets/*.min.css', './dist/owl.carousel.min.js']
+    },
+
+    "Detect.js": {
+        "main": ['./detect.min.js']
+    },
+
+    "jquery.stellar": {
+        "main": ['./src/jquery.stellar.js']
+    },
+
+    "mixitup": {
+        "main": ['./build/jquery.mixitup.min.js']
+    },
+
+    "css-hamburgers": {
+        "main": ['./dist/hamburgers.min.css']
+    },
+
+    "photoswipe": {
+        "main": ['./dist/**/*']
+    },
+
+    "swipebox": {
+        "main": ['./src/**/*']
     }
 };
+
+/********************************************************************/
+/*IMG-SET************************************************************/
+/********************************************************************/
+
+gulp.task('imgSet', function () {
+    return gulp.src(path.src.imgSet)
+        .pipe(plugins.responsive({
+            '*.*': [{
+                width: 544,
+                rename: {suffix: '-xs'},
+            }, {
+                width: 768,
+                rename: {suffix: '-sm'},
+            }, {
+                width: 992,
+                rename: {suffix: '-md'},
+            }, {
+                width: 1200,
+                rename: {suffix: '-lg'},
+            }, {
+                width: 1920,
+                rename: {suffix: '-xl'},
+            }]
+        }, {
+            quality: 100,
+            progressive: true,
+            withMetadata: false,
+        }))
+        .pipe(gulp.dest(path.dist.imgSet));
+});
 
 /********************************************************************/
 /*SERVER*************************************************************/
 /********************************************************************/
 
-gulp.task('server', function() {
-      return browserSync.init({
-            server: { baseDir: './dist' },
-            logPrefix: nameProject,
-            logFileChanges: false,
-            reloadDelay: 1000,
-            ghostMode: false,
-            online: false
+gulp.task('server', function () {
+    return browserSync.init({
+        server: {baseDir: './dist'},
+        logPrefix: nameProject,
+        logFileChanges: false,
+        reloadDelay: 1000,
+        ghostMode: false,
+        online: true
     });
 });
-
-
-/********************************************************************/
-/*MOCKUPS************************************************************/
-/********************************************************************/
-
-gulp.task('mockup', function() {
-  return new Pageres({delay: 5})
-        .src(siteScreen, resolutions, {crop: true})
-        .dest(path.dist.screen)
-        .run()
-        .then(() => console.log('Готово'));
-});
-
 
 /********************************************************************/
 /*VENDOR*************************************************************/
 /********************************************************************/
 
-    gulp.task('vendor', function() {
-    return gulp.src(mainBowerFiles({"overrides": bowerConfig}), { base: './bower_components' })
+gulp.task('vendor', function () {
+    return gulp.src(mainBowerFiles({"overrides": bowerConfig}), {base: './bower_components'})
         .pipe(gulp.dest(path.dist.vendor))
 });
 
@@ -122,7 +178,7 @@ gulp.task('html', function () {
 /*JS*****************************************************************/
 /********************************************************************/
 
-gulp.task('js',  function() {
+gulp.task('js', function () {
     return gulp.src(path.src.js)
         .pipe(plugins.newer(path.dist.js))
         .pipe(plugins.sourcemaps.init())
@@ -144,7 +200,7 @@ gulp.task('style', function () {
         .pipe(plugins.sourcemaps.init())
         .pipe(plugins.sass.sync().on('error', plugins.sass.logError))
         .pipe(plugins.autoprefixer({browsers: ['last 10 versions'], cascade: false}))
-        // .pipe(plugins.csso())
+        .pipe(plugins.csso())
         .pipe(plugins.rename({suffix: '.min'}))
         .pipe(plugins.sourcemaps.write())
         .pipe(gulp.dest(path.dist.css))
@@ -190,7 +246,7 @@ gulp.task('clean', function () {
 });
 
 /********************************************************************/
-/*Watch***************************************************************/
+/*WATCH**************************************************************/
 /********************************************************************/
 
 gulp.task('watch', function () {
@@ -199,14 +255,34 @@ gulp.task('watch', function () {
     gulp.watch(path.watch.js,         gulp.series('js'));
     gulp.watch(path.watch.html,     gulp.series('html'));
     gulp.watch(path.watch.img,       gulp.series('img'));
+    gulp.watch(path.watch.imgSet, gulp.series('imgSet'));
     gulp.watch(path.watch.vendor, gulp.series('vendor'));
 });
 
+gulp.task('psi', function () {
+    return psi('google.com').then(data => {
+        console.log(data.ruleGroups.SPEED.score);
+        console.log(data.pageStats);
+    });
+});
+
 /********************************************************************/
-/*Default************************************************************/
+/*DEFAULT************************************************************/
 /********************************************************************/
 
 gulp.task('default',
-    gulp.series('clean','style', 'js', 'vendor', 'fonts', 'json', 'img', 'html',
-        gulp.parallel ('server', 'watch'))
+    gulp.series('clean', 'img', 'imgSet', 'style', 'js', 'vendor', 'fonts', 'json', 'html',
+        gulp.parallel('server', 'watch'))
 );
+
+
+
+
+/*
+
+// get the PageSpeed Insights report
+psi('http://localhost:3000').then(data => {
+    console.log(data.ruleGroups.SPEED.score);
+    console.log(data.pageStats);
+});
+*/
